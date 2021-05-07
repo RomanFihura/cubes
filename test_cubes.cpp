5575028bc4074d1e7d27b393ed0aa03f7b9bebee
Roman Fihura <romanfigura120@gmail.com>
2021-05-07 11:54:42 +0300

    test edit

--- test_cubes.cpp
+++ test_cubes.cpp
@@ -6,11 +6,16 @@ TEST(cubes_method2, most_frequent_7)
 {
     std::vector<int> numbers;
     uint32_t temp = 0;
+    uint32_t runs = 500;
+    for (uint32_t i = 0; i < runs; i++)
+    {
         for (uint32_t i = 0; i < 1000; i++)
         {
             numbers.push_back(attempt());
         }
         temp += mostFrequent2(numbers);
-
-    EXPECT_EQ(7, 7);
+        numbers.clear();
+    }
+    double actualrelation = (static_cast<double>(temp) / runs);
+    EXPECT_NEAR(actualrelation, 7, 0.2);
 }
